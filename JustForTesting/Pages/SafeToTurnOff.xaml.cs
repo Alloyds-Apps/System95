4439918c086447240e37d05ff84e94259080d4f4
Alloyd <kirillpravikov@outlook.com>
2024-07-05 21:36:00 +0300

    Another big changes

---
+++ JustForTesting/Pages/SafeToTurnOff.xaml.cs
@@ -0,0 +1,36 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace System95.JustForTesting.Pages
+{
+    public sealed partial class SafeToTurnOff : Page
+    {
+        public SafeToTurnOff()
+        {
+            this.InitializeComponent();
+        }
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(3000);
+            this.TurnOn.Visibility = Visibility.Visible;
+        }
+        private void TurnOn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GameUI), null, new SuppressNavigationTransitionInfo());
+        }
+    }
+}
--- JustForTesting/System95OS.xaml.cs
+++ JustForTesting/System95OS.xaml.cs
@@ -4,22 +4,31 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System95.JustForTesting.Pages;
+using System.Threading.Tasks;
 
 namespace System95.JustForTesting
 {
     public sealed partial class System95OS : Page
     {
+        public bool IsSingleTap;
+        public static System95OS Current;
+        private bool isPressed = true;
         public System95OS()
         {
             this.InitializeComponent();
+            Current = this;
         }
         private void StartButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -28,6 +37,44 @@ namespace System95.JustForTesting
         private void StartButton_Unchecked(object sender, RoutedEventArgs e)
         {
             this.StartMenu.Visibility = Visibility.Collapsed;
+        }
+        private void Taskbar_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed == true)
+            {
+                if (isPressed)
+                {
+                    this.StartButton.IsChecked = false;
+                    this.StartMenu.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+        private void Taskbar_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed == true)
+            {
+                isPressed = true;
+            }
+        }
+        private void ShutDown_Click(object sender, RoutedEventArgs e)
+        {
+            this.StartButton.IsChecked = false;
+            this.StartMenu.Visibility = Visibility.Collapsed;
+            this.ShutDownWindow.Visibility = Visibility.Visible;
+        }
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShutDownWindow.Visibility = Visibility.Collapsed;
+        }
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShutDownWindow.Visibility = Visibility.Collapsed;
+            this.Frame.Navigate(typeof(PleaseWaitRestart), null, new SuppressNavigationTransitionInfo());
+        }
+        private void ShutDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ShutDownWindow.Visibility = Visibility.Collapsed;
+            this.Frame.Navigate(typeof(PleaseWaitShutDown), null, new SuppressNavigationTransitionInfo());
         }
     }
 }
